a11efad29259f8c320ca01f9453a5c94c138d04b
yassin19 <yassineldeeb94@gmail.com>
2021-01-30 16:42:25 +0200

    mongoose can't connect

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
     })
     console.log(connect.connection.host.cyan.underline.bold)
   } catch (err) {
