1873280a82e9ec4f7b22125258fec8c3ab75ecb5
yassin19 <yassineldeeb94@gmail.com>
2021-03-30 19:17:05 +0200

    heroku serve files

--- backend/server.js
+++ backend/server.js
@@ -40,12 +40,11 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")))
 
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   })
@@ -53,6 +52,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.send("The API is running!")
   })
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 }
 
 app.use(notFoundRouter)
