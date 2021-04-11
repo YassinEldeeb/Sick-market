d43837e7bf6e1080caf9a530567f64be0dbac485
yassin19 <yassineldeeb94@gmail.com>
2021-04-11 21:47:55 +0200

    configure the server for prerender

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
-import dotenv from "dotenv"
-import connectDB from "./db/mongoose.js"
-import colors from "colors"
-import productRouter from "./routes/productRoute.js"
-import { errRouter, notFoundRouter } from "./middleware/errMiddleware.js"
-import userRouter from "./routes/userRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-import couponRouter from "./routes/couponRoute.js"
-import bodyparser from "body-parser"
-import rateLimit from "express-rate-limit"
-import path from "path"
-import express from "express"
-import http from "http"
-import SocketService from "./webSockets/socketService.js"
-import wakeUpDyno from "./utils/wakeUpDyno.js"
-
-import Category from "./models/category.js"
+import dotenv from 'dotenv'
+import connectDB from './db/mongoose.js'
+import colors from 'colors'
+import productRouter from './routes/productRoute.js'
+import { errRouter, notFoundRouter } from './middleware/errMiddleware.js'
+import userRouter from './routes/userRoute.js'
+import orderRouter from './routes/orderRoute.js'
+import couponRouter from './routes/couponRoute.js'
+import bodyparser from 'body-parser'
+import rateLimit from 'express-rate-limit'
+import path from 'path'
+import express from 'express'
+import http from 'http'
+import SocketService from './webSockets/socketService.js'
+import wakeUpDyno from './utils/wakeUpDyno.js'
+import prerender from 'prerender-node'
+import Category from './models/category.js'
 
 const app = express()
 const server = http.Server(app)
 
 app.use(express.json())
 dotenv.config()
+app.use(prerender.set('prerenderToken', [process.env.PRERENDER_TOKEN]))
 
 connectDB()
 
@@ -29,41 +30,41 @@ app.use(bodyparser.urlencoded({ extended: true }))
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
   max: 1,
-  message: { message: "Try again in 60 seconds" },
+  message: { message: 'Try again in 60 seconds' },
 })
 
-app.use("/api/products", productRouter)
-app.use("/api/users/getNewSecurityCode", apiLimiter)
-app.use("/api/users", userRouter)
-app.use("/api/orders", orderRouter)
-app.use("/api/coupons", couponRouter)
+app.use('/api/products', productRouter)
+app.use('/api/users/getNewSecurityCode', apiLimiter)
+app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
+app.use('/api/coupons', couponRouter)
 
-app.get("/api/config/paypal", (req, res) =>
+app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 } else {
-  app.get("/", (req, res) => {
-    res.send("The API is running!")
+  app.get('/', (req, res) => {
+    res.send('The API is running!')
   })
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 }
 
 app.use(notFoundRouter)
 app.use(errRouter)
 
 const port = process.env.PORT || 5000
-const DYNO_URL = "https://sick-market.herokuapp.com"
+const DYNO_URL = 'https://sick-market.herokuapp.com'
 server.listen(port, () => {
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'production') {
     wakeUpDyno(DYNO_URL)
   }
 
@@ -72,4 +73,4 @@ server.listen(port, () => {
   )
 })
 
-app.set("socketService", new SocketService(server))
+app.set('socketService', new SocketService(server))
