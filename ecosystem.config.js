8502689e951b2f15dd3ce395a81cf46de9d8d26f
Yassin Eldeeb <72823042+YassinEldeeb@users.noreply.github.com>
2021-04-27 15:16:26 +0200

    adding pm2 for scalling

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'server',
+      script: './server.js',
+      instances: 'max',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
