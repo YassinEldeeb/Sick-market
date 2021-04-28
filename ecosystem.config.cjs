0bf7fb9054e5a5932ecff113567f68d7fa728933
Yassin Eldeeb <72823042+YassinEldeeb@users.noreply.github.com>
2021-04-29 00:44:25 +0200

    pm2 test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'Sick-market',
       script: 'backend/server.js',
-      instances: 'max',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
+      watch: '.',
     },
   ],
 }
