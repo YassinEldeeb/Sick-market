5e8a54501d9cbbd598fdf7157d7a2df7682fb0f3
Yassin Eldeeb <72823042+YassinEldeeb@users.noreply.github.com>
2021-05-05 08:54:53 +0200

    Last one I promise

--- frontend/src/clientSocket/socket.js
+++ frontend/src/clientSocket/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
-const socket = io('https://sickmarket.ml', {
-  transports: ['websocket'],
+const socket = io({
+  transports: ['polling', 'websocket'],
 })
 
 export default socket
