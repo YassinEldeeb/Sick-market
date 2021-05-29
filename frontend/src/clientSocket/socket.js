91ce15f7e1121472651ed563cb77a635cfc2902f
Yassin Eldeeb <72823042+YassinEldeeb@users.noreply.github.com>
2021-05-29 19:57:17 +0200

    client socket connect

--- frontend/src/clientSocket/socket.js
+++ frontend/src/clientSocket/socket.js
@@ -1,5 +1,11 @@
 import { io } from 'socket.io-client'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const socket = io('http://localhost:5000')
+const socket = io(
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : 'https://sickmarket.ml/client'
+)
 
 export default socket
